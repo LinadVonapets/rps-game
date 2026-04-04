2cafa3bd039055ef7df3741dc4bc04c66b3ac735
Daniil Stepanov <10022004d@gmail.com>
2026-04-04 11:52:37 +0300

    rps: resmngr: Fix syntax errors
    * Add unload methods with reference count check.
    * Fix syntax errors: used id and resource instead of p_id and resources.
    * Change resource name res for low confusion with resources.

--- src/ResourceManager.hpp
+++ src/ResourceManager.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <iostream>
 
 #include <SFML/Graphics/Texture.hpp>
 
@@ -13,7 +14,6 @@ class ResourceManager
 {
 protected:
 	std::unordered_map<std::string, std::shared_ptr<T>> resources;
-	std::string resource_path = "assets";
 
 public:
 	virtual ~ResourceManager() = default;
@@ -27,14 +27,14 @@ public:
 			return it->second;
 		}
 
-		auto resource = std::make_shared<T>();
-		if(!load_from_file(resource.get(), resource_path + p_filename))
+		auto res = std::make_shared<T>();
+		if(!load_from_file(res.get(), p_filename))
 		{
-			throw std::runtime_error("Failed to load: " + filename)
+			throw std::runtime_error("Failed to load: " + p_filename);
 		}
 
-		resource[id] = resource;
-		return resource;
+		resources[p_id] = res;
+		return res;
 	}
 
 	std::shared_ptr<T> get(const std::string& p_id)
@@ -45,6 +45,18 @@ public:
 			return it->second;
 		}
 		return nullptr;
+	}
+
+	void unload(const std::string& p_id, bool force=false)
+	{
+		auto it = resources.find(p_id);
+		long use_count = it->second.use_count();
+		if (use_count > 1 && !force)
+		{
+			std::cerr << "Failed: Cannot delete: ";
+			std::cerr << "The resource " << p_id << " is used by " << use_count << " objects" << std::endl;
+		}
+		resources.erase(it);
 	}
 
 	virtual bool load_from_file(T* p_resource, const std::string& p_path) = 0;
